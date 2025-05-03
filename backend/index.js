ae71ee11f11f0b74ab96cf94cb3d57178b23b9cf
santosh-karki <karki_santosh@outlook.com>
2025-05-03 23:13:09 +1000

    s

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,7 @@ const PORT = process.env.PORT || 3000;
 const __dirname = path.resolve();
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+//
 
 app.use(express.json()); 
 app.use(cookieParser()); 
